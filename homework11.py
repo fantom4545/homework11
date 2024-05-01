f01294d44065a14cd8a5a98e122d010ab1eec973
fantom4545 <166008514+fantom4545@users.noreply.github.com>
2024-05-01 22:20:26 +0700

    Add files via upload

---
+++ homework11.py
@@ -0,0 +1,17 @@
+def test(*params):
+    print(*params)
+    print(params)
+
+
+test(1, 2, 2, 2)
+
+
+def sum(n):
+    if n == 0:
+       return 0
+    else:
+        return n + sum(n - 1)
+
+
+print(sum(7))
+
